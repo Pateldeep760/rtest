69e89059552ef844dc0ade341ab2e04e4f788dd2
Pateldeep760 <pateldeep760@gmail.com>
2025-03-18 23:29:11 +0530

    deep

---
+++ src/Header/Header.jsx
@@ -0,0 +1,18 @@
+import './Header.css'
+
+// const Header=()=> {
+
+//     return(
+//         <h1>Header111</h1>
+//     )
+// }
+
+
+function Header(){
+    return(
+        <h1>Header111</h1>
+    )
+    
+}
+
+export default Header 
